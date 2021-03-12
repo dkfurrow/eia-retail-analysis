b02918e1375c0a78feb02d71712a38f45ba4d3dc
dale <dkfurrow@gmail.com>
2021-03-12 11:43:49 -0600

    made adjustements to cleaning and parsing of raw data

--- eia_retail_analysis1.py
+++ eia_retail_analysis1.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-"""Example Multivariate Regression
+"""Analyze EIA retail sales data
+   Python Version: 3.83
 """
 # %%
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as ml
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
 from datetime import datetime, date
 from tabulate import tabulate
 import os
@@ -17,92 +16,88 @@ from pprint import pprint
 from pathlib import Path
 from pprint import pprint
 import glob
-from pathlib import Path
 from dateutil.parser import parse
+# specific libraries to download from web
+from zipfile import ZipFile
+from collections import OrderedDict
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.width', 1000)
 pd.options.display.float_format = '{:,.2f}'.format
 #%%
-# specific libraries to download from web
-from pandas.tseries.offsets import Day
-from io import BytesIO
-from zipfile import ZipFile
-import requests
-from jinja2 import Template
-import csv
-from collections import OrderedDict
-#%%
-DOWNLOAD_ROOT = "D:\\DKF\\Data\\EIA"  # Put zipfiles in this directory
+DOWNLOAD_ROOT = "./data"  # Put zipfiles in this directory
 zipped_dict = {}
 zipped_files = glob.glob(str(Path(DOWNLOAD_ROOT, 'f86*.zip')))
+print("Data will be extracted from the following files")
 pprint(zipped_files)
 #%%
+# We want to extract tables 6-10, which have meanings as follows
 file_data_class = dict([('table6', 'residential'), ('table7', 'commercial'), ('table8', 'industrial'),
                         ('table9', 'transportation'), ('table10', 'all')])
+# EIA switched the order of revenue and sales columns between 2007 and 2008, hence we have two units dictionaries
 header_units_2008_forward = OrderedDict(zip(['Entity', 'State', 'Ownership', 'Customers', 'Sales', 'Rev', 'AvgPrc'],
                                             ['NA', 'NA', 'NA', 'Count', 'MWH',  '$000', 'cPerKWH']))
 header_units_2007_back = OrderedDict(zip(['Entity', 'State', 'Ownership', 'Customers', 'Rev', 'Sales', 'AvgPrc'],
                                             ['NA', 'NA', 'NA', 'Count', '$1000',  'MWH', 'cPerKWH']))
-
+# These are the desired header types
 header_types = OrderedDict(zip(['Entity', 'State', 'Ownership', 'Customers', 'Sales',  'Rev', 'AvgPrc'],
                                ['category', 'category', 'category', 'int64', 'float64',  'float64', 'float64']))
 data_years = []
 all_data = None
+print("fetching EIA data from zipped files...")
 for zipped_file in zipped_files:
     f = ZipFile(zipped_file)
     data_year = int(Path(zipped_file).stem[-4:])
     data_years.append(data_year)
-    print("fetching data year {0:d}".format(data_year))
+    print("fetching all tablses data year {0:d}".format(data_year))
     for filename in f.namelist():
         table_stem = Path(filename).stem
         if table_stem in file_data_class.keys():
             data_class = file_data_class[table_stem]
-            print("fetching {0}".format(data_class))
+            print("fetching {0} category for {1:d}".format(data_class, data_year))
             with f.open(filename, 'r') as g:
                 df = pd.read_excel(g, na_values=['.', '', '-', '*'])
-                # print("before reduce")
-                # print(df.head(n=8))
+                # we want to truncate the dataframe using the first valid state index
                 # find the first 'Alaska Entry', (if not zero then there are extraneous rows in df)
                 first_valid_index = df.iloc[:, 1].eq('AK').idxmax()
                 if first_valid_index == 0:
+                # If there's no Alaska, then first row is Arkansas [transportation]
                     first_valid_index = df.iloc[:, 1].eq('AR').idxmax()
                 if first_valid_index == 0:
                     first_valid_index = df.iloc[:, 1].eq('CA').idxmax()
-                print("First Valid Index {0:d}".format(first_valid_index))
                 if first_valid_index != 0:
-                    df = df.iloc[first_valid_index:, :7]
-                    # print("after reduce")
-                    # print(df.head())
+                    df = df.iloc[first_valid_index:, :7]  # some spreadsheets have extraneous columns
+                # as indicated above, EIA switched the column order in
                 if data_year >= 2008:
                     df.columns = header_units_2008_forward.keys()
                 else:
                     df.columns = header_units_2007_back.keys()
                 df = df.convert_dtypes()
-                print("after reformat")
+                print("selected dataframe excerpt")
                 print(df.head())
-                convert_specials = ['Customers', 'Sales', 'Rev']  # convert to float
-                for convert_special in convert_specials:
-                    df[convert_special] = df[convert_special].astype('float64')
+                convert_numerics = df.columns.tolist()[3:]  # convert numeric columns toi float
+                for convert_numeric in convert_numerics:
+                    df[convert_numeric] = pd.to_numeric(df[convert_numeric], errors='coerce')
+                    df[convert_numeric] = df[convert_numeric].astype('float64')
+                # insert customer class, data year
                 df.insert(loc=0, column='CustClass', value=data_class)
                 df.insert(loc=0, column='Year', value=data_year)
+                # append to get full dataset
                 all_data = pd.concat([all_data, df]) if all_data is not None else df.copy()
+print("cleaning extraneous null values...")
+all_data = all_data[all_data.State.notnull()]  # these are all null rows, 2004-2009
+all_data = all_data[(all_data.Rev != 0.) & (all_data.Sales != 0.) & (all_data.Customers != 0.)]
+all_data.reset_index(inplace=True)
+print("all eia revenue and sales data cleaned and assembled: {0:,} rows".format(len(all_data)))
 
 #%%
-# fix AvgPrice
-avg_price_cleaned = []
-for val in all_data['AvgPrc'].values:
-    if isinstance(val, float) or isinstance(val, int):
-        avg_price_cleaned.append(float(val))
-    else:
-        try:
-            avg_price_cleaned.append(float(val.strip()))
-        except:
-            avg_price_cleaned.append(float('nan'))
-all_data['AvgPrc'] = avg_price_cleaned
-#%%
-# make check col
-all_data['check_centsPerKwh'] = all_data['Rev'] / all_data['Sales'] * 100.
+# make check column, compare to extracted average price, print useful data summaries
+print("Make check columns, examine reults")
+all_data['RevPerSales'] = all_data['Rev'] / all_data['Sales'] * 100.
+all_data['ChkAvgPrc'] = np.abs(all_data['AvgPrc'] - all_data['RevPerSales'])
+num_cols_not_tying = len(all_data[(all_data.ChkAvgPrc > 0.01)])
+print("AvgPrc equal to RevPerSales except in {0:d} instances where AvgPrc was listed as zero"
+      .format(num_cols_not_tying))
 # make any adjustments here.
 # all_data = all_data[all_data['Entity'].str.find('Adjustment') == -1]
 
